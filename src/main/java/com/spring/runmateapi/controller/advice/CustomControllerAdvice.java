593271019ee15751dcaf1d8d8323a73c7a9de2f0
gustmdakstp <hs820610@gmail.com>
2026-08-28 12:42:38 +0900

    running으로 커밋

--- src/main/java/com/spring/runmateapi/controller/advice/CustomControllerAdvice.java
+++ src/main/java/com/spring/runmateapi/controller/advice/CustomControllerAdvice.java
@@ -23,17 +23,17 @@ public class CustomControllerAdvice {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("msg",e.getMessage()));
     }
 
-    // 401 Unauthorized는 요청한 사용자의 인증 정보가 없거나 올바르지 않아 인증에 실패한 경우 반환하는 HTTP 상태 코드
-    // 로그인 인증 실패 처리
-//    @ExceptionHandler(AuthenticationException.class)
-//    protected  ResponseEntity<?> handleAuthenticationException(AuthenticationException e) {
-//        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-//                .body(Map.of("msg","아이디 또는 비밀번호가 올바르지 않습니다."));
-//    }
-//
-//    @ExceptionHandler(JwtException.class)
-//    protected ResponseEntity<?> handleJwtException(JwtException e) {
-//        return  ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-//                .body(Map.of("msg","유효하지 않은 토큰입니다."));
-//    }
+//     401 Unauthorized는 요청한 사용자의 인증 정보가 없거나 올바르지 않아 인증에 실패한 경우 반환하는 HTTP 상태 코드
+//     로그인 인증 실패 처리
+    @ExceptionHandler(AuthenticationException.class)
+    protected  ResponseEntity<?> handleAuthenticationException(AuthenticationException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(Map.of("msg","아이디 또는 비밀번호가 올바르지 않습니다."));
+    }
+
+    @ExceptionHandler(JwtException.class)
+    protected ResponseEntity<?> handleJwtException(JwtException e) {
+        return  ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(Map.of("msg","유효하지 않은 토큰입니다."));
+    }
 }
